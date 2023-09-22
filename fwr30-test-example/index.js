efba4633590e27b59c9467d7022fadc5c171e6b3
Raskolnikov <jerry.tann@endress.com>
2023-09-22 22:54:42 +0800

    🚀

--- fwr30-test-example/index.js
+++ fwr30-test-example/index.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk');
 
-module.exports.handler = async (event) => {
+module.exports.handler = async (event, context) => {
   console.log("event stringified is ", JSON.stringify(event));
+  console.log("context stringified is ", JSON.stringify(context));
   return {
     statusCode: 200,
     body: JSON.stringify(
